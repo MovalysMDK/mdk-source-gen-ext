c8f00f3616d0fdf02979a4171e316b9b794e03f1
Simon Bernardin <simon.bernardin@soprasteria.com>
2015-10-29 11:07:55 +0000

    Ajout du widgetVariant dans le VersionHandler

---
+++ commons/src/main/java/com/adeuza/movalysfwk/mf4mdd/commons/utils/Widget.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2010 Sopra Steria Group (movalys.support@soprasteria.com)
+ *
+ * This file is part of Movalys MDK.
+ * Movalys MDK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Movalys MDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Movalys MDK. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.adeuza.movalysfwk.mf4mdd.commons.utils;
+
+import com.a2a.adjava.utils.ItfWidget;
+
+public enum Widget implements ItfWidget {
+
+	LEGACY_WIDGET("legacywidget"), 
+	MDK_WIDGET("mdkwidget");
+
+	private String version;
+
+	private Widget(String version) {
+		this.version = version;
+	}
+
+	@Override
+	public String getStringWidget() {
+		return this.version;
+	}
+
+	@Override
+	public ItfWidget getLastWidget() {
+		return MDK_WIDGET;
+	}
+
+}
